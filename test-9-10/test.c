3f653c8be71b91e9a24487d74507245accde0f39
ice-cojy <18788313697@163.com>
2023-09-14 18:40:30 +0800

    Add files via upload

---
+++ test-9-10/test.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    int l = 0;
+    printf("gagdg\n");
+   while (l<=10);
+   {
+    printf("fight\n");
+    l++;
+   }
+   if (l>10);
+   {
+   printf("nice\n");
+   }
+    return 0;
+}
