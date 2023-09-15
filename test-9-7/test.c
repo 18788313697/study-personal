c952168938a597be52f9444e27b531669fef1c6f
ice-cojy <18788313697@163.com>
2023-09-15 23:00:23 +0800

    测试

---
+++ test-9-7/test.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+int arr[10] = {1,2,3,4,5,6,7,8,9,10};  
+int line = 0 ;
+while (line<10)
+{
+    
+    printf("%d\n",arr[line]);
+    line++;
+}
+printf("hello world\n");
+return 0;
+}
+
+
